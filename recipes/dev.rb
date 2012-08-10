3b6228ba3e32b4ca95a571ea69cffcb691726b29
Romain Champourlier <pro@rchampourlier.com>
2012-08-10 11:11:30 +0200

    Separating client from dev tools (for Ubuntu)

--- recipes/dev.rb
+++ recipes/dev.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: postgresql
-# Recipe:: dev
+# Recipe:: client
 #
 # Author:: Romain Champourlier (<romain@softr.li>)
 # Author:: Joshua Timberman (<joshua@opscode.com>)
@@ -22,11 +22,24 @@
 
 pg_packages = case node['platform']
 when "ubuntu","debian"
-  %w{postgresql-client}
+  %w{libpq-dev ruby-dev}
+when "fedora","suse","amazon"
+  %w{postgresql-devel}
+when "redhat","centos","scientific"
+  case
+  when node['platform_version'].to_f >= 6.0
+    %w{postgresql-devel}
+  else
+    [ "postgresql#{node['postgresql']['version'].split('.').join}-devel" ]
+  end
 end
 
 pg_packages.each do |pg_pack|
   package pg_pack do
-    action :install
-  end
+    action :nothing
+  end.run_action(:install)
 end
+
+gem_package "pg" do
+  action :nothing
+end.run_action(:install)
