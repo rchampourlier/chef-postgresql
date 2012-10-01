59c71b650fad903942bc2b84338e2bd55295c705
Romain Champourlier <pro@rchampourlier.com>
2012-10-01 11:16:56 +0200

    Corrected mistake in server_debian recipe code

--- recipes/server_debian.rb
+++ recipes/server_debian.rb
@@ -31,12 +31,13 @@ else # > 8.3
   node.default[:postgresql][:ssl] = "true"
 end
 
-pg_packages = case node['platform']
-if node[:postgresql][:version].to_f < 9
-  'postgresql-server'
-else # should work for 9.1 and 9.2
-  "postgresql-server-#{node[:postgresql][:version]}"
-end
+pg_packages = (
+  if node[:postgresql][:version].to_f < 9
+    'postgresql-server'
+  else # should work for 9.1 and 9.2
+    "postgresql-server-#{node[:postgresql][:version]}"
+  end
+)
 
 pg_packages.each do |pg_pack|
   package pg_pack do
